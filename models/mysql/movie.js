ecb70a28a9a8ad340f6212b2b9e2dd2ff2a67dbb
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-04-03 17:09:22 -0300

    refactor: cambio de imports en controllers/movies.js

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -1,16 +1,22 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 const config = {
   host: "localhost",
   user: "root",
+  port: 3306,
   password: "",
   database: "moviesdb",
 };
 
-const connection = mysql.createConnection(config);
+const connection = await mysql.createConnection(config);
 
 export class MovieModel {
-  static async getAll({ genre }) {}
+  static async getAll({ genre }) {
+    const result = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie;`
+    );
+    console.log(result);
+  }
 
   static async getById({ id }) {}
 
